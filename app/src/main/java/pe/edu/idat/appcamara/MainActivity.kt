9d8cdfdf5c9232c01dbf2ac9277d016cd7b44ffa
Viccho2828 <kikekollizion@gmail.com>
2024-10-31 19:45:10 -0500

    validación de permisos

--- app/src/main/java/pe/edu/idat/appcamara/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/appcamara/MainActivity.kt
@@ -1,9 +1,13 @@
 package pe.edu.idat.appcamara
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pe.edu.idat.appcamara.databinding.ActivityMainBinding
@@ -27,5 +31,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
     private fun tomarFoto() {
+        if(validarPermiso()){
+            Toast.makeText(applicationContext,
+                "Puedo tomar foto", Toast.LENGTH_LONG).show()
+        }else{
+            solicitarPermiso()
+        }
+    }
+    private fun intentCamara(){
+
+    }
+
+    private fun validarPermiso():Boolean{
+        val result = ContextCompat.checkSelfPermission(
+            applicationContext,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+    private fun solicitarPermiso(){
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1988)
     }
 }
